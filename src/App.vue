d16677141d2ae67c861a24e07f6f4fe1f52a24c4
hujiajia <hu.jiajia@qq.com>
2021-11-15 10:53:40 +0800

    完成项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div>
+  <!-- 使用模块A的username -->
+  <p>{{$store.state.moduleA.username}}</p>
+  <!-- 使用模块A的getters -->
+  <p>{{$store.getters.changeName}}</p>
+  <!-- 使用模块B的username -->
+  <p>{{$store.state.moduleB.username}}</p>
+  <!-- 使用模块B的getters -->
+  <p>{{$store.getters['moduleB/changeName']}}</p>
+  <button @click="updateName">修改username</button>
+  <button @click="actionsFn">actionsFn</button>
+</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  setup () {
+    // 使用vuex仓库
+    const store = useStore()
+    const updateName = () => {
+      // 提交B模块的修改
+      store.commit('moduleB/updateName')
     }
+    const actionsFn = () => {
+      // 调用B模块的actions
+      store.dispatch('moduleB/updateName')
+    }
+    return { updateName, actionsFn }
   }
+
 }
+</script>
+
+<style>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,78 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
+// A模块
+const moduleA = {
+  // 子模块state建议写成函数
+  state: () => {
+    return {
+      username: 'moduleA'
+    }
+  },
+  getters: {
+    changeName (state) {
+      return state.username + 'AAA'
+    }
+  }
+}
+// B模块
+const moduleB = {
+  // 带命名空间的模块
+  namespaced: true,
+  state: () => {
+    return {
+      username: 'moduleB'
+    }
+  },
+  getters: {
+    changeName (state) {
+      return state.username + 'BBB'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.username = 'BBB' + state.username
+    }
   },
   actions: {
-  },
-  modules: {
+    updateName ({ commit }) {
+      setTimeout(() => {
+        commit('updateName')
+      }, 1000)
+    }
+
   }
+}
+export default createStore({
+  modules: {
+    moduleA,
+    moduleB
+  }
+
 })
+
+// vue2.0 new Vuex.store({})sssssssssss
+// vue3.0 创建仓库createStore({})
+// export default createStore({
+//   state: {
+//     username: 'zs'
+//   },
+//   getters: {
+//     newName (state) {
+//       return state.username + '!!!'
+//     }
+//   },
+//   mutations: {
+//     updateName (state) {
+//       state.username = 'ls'
+//     }
+//   },
+//   actions: {
+//     updateName (context) {
+//       setTimeout(() => {
+//         context.commit('updateName')
+//       }, 1000)
+//     }
+//   },
+//   modules: {
+//   }
+// })
